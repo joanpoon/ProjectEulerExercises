fbb01634aa1f45e7397da9ba60fe85a7a7b4c32f
Joan Poon <36224086+joanpoon@users.noreply.github.com>
2018-11-07 22:55:43 -0800

    Add files via upload
    Solved the Towers of Hanoi in C#

---
+++ TowersOfHanoi/TowersOfHanoi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TowersOfHanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfDisk = 5;
+            char fromRod = 'A';
+            char toRod = 'C';
+            char tempRod = 'B';
+
+            solveHanoi(numOfDisk, fromRod, toRod, tempRod);
+        }
+
+        //recursive method to solve Towers of Hanoi with {n} disk
+        static void solveHanoi(int n, char from, char to, char temporary)
+        {
+            if (n > 0)
+            {
+                solveHanoi(n - 1, from, temporary, to);
+                Console.WriteLine($"Moving disk #{n} from {from} to {to} ...");
+                solveHanoi(n - 1, temporary, to, from);
+            }
+        }
+    }
+}
