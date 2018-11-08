bbd5d0c54b8cf17a38e9364dc8652bdb5a459daf
Joan Poon <36224086+joanpoon@users.noreply.github.com>
2018-11-07 23:17:34 -0800

    Add files via upload
    Problem 67:
    By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
    
    3
    7 4
    2 4 6
    8 5 9 3
    
    That is, 3 + 7 + 4 + 9 = 23.
    
    Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.
    
    NOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 299 altogether! If you could check one trillion (1012) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)

---
+++ MaxPathSum/MaxPathSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace MaxPathSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("triangletext.txt");
+            int N = lines.Length;
+            int[,] numbers = new int[N, N];
+
+            //split the numbers in string[] lines, and parse them to int array
+            for (int i = 0; i < N; i++)
+            {
+                string[] snumbers = lines[i].Split(' ');
+                for (int j = 0; j <= i; j++)
+                    numbers[i, j] = int.Parse(snumbers[j]);
+            }
+
+
+            for (int i = N - 2; i >= 0; i--)
+                for (int j = 0; j <= i; j++)
+                    //pick the larger number and add it to the current number
+                    numbers[i, j] += Math.Max(numbers[i + 1, j], numbers[i + 1, j + 1]);
+
+            //the current max sum would be the number at numbers[0,0]
+            Console.WriteLine(numbers[0, 0]);
+        }
+    }
+}
